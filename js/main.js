9492c75288ec72dfda36fb5a0d3b81df98e23178
estebanCastr0 <83050986+estebanCastr0@users.noreply.github.com>
2021-09-16 19:18:51 -0500

    Se muestran los campos  en la ventana

--- js/main.js
+++ js/main.js
@@ -1,21 +1,9 @@
 
 document.querySelector("#fetchbuton").addEventListener("click", fetchDictionary);
 
+
 var url ="https://dog.ceo/api/breeds/list/all";
-
-
-/*
-fetch('https://api.dictionaryapi.dev/api/v2/entries/en/hello')
-  .then(response => response.json())
-  .then(data => {
-    //guarda todo lo que esta en data.message en una lista
-    //map hace iteracion y me devuelve bred
-    //Object.keys(data.definition).map((definition)=>{
-    //    console.log(definition);
-   //});
-    console.log(data.origin);
-  });
-  */
+var link
 
 function fetchDictionary(){
   var word=document.querySelector("#wordInput").value;
@@ -24,55 +12,102 @@ function fetchDictionary(){
   var keyMeanings =0;
   var keyDefinitions =0;
   console.log(word);
-
-  fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word)
-  .then(response =>response.json())
-  .then(data => {
-    Object.keys(data).map((data)=>{
-      //console.log("data"+data);
-      keyData++;
-    })
-    console.log("numero"+keyData);
-    for(let i=0;i<keyData;i++){
-      Object.keys(data[i]["meanings"]).map((meanings)=>{
-        //console.log(i+"meanings"+meanings);
-        keyMeanings++;
+  if(word!=""){
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word)
+    .then(response =>response.json())
+    .then(data => {
+      Object.keys(data).map((data)=>{
+        //console.log("data"+data);
+        keyData++;
       })
-      for(let j=0;j<keyMeanings;j++){
-        Object.keys(data[i]["meanings"][j]["definitions"]).map((definitions)=>{
-          if(keyDefinitions==0){
-            console.log("partOfSpeech");
-            console.log(data[i]["meanings"][j]["partOfSpeech"]);
-          }
-          //console.log(i+"definitions"+definitions);
-          keyDefinitions++;
-        })
-        
-        for(let k=0;k<keyDefinitions;k++){
-            console.log("Definiciones");
-            console.log(data[i]["meanings"][j]["definitions"][k]["definition"]);
-            if(data[i]["meanings"][j]["definitions"][k]["example"]!=undefined){
-            console.log("Ejemplos");
-            console.log(data[i]["meanings"][j]["definitions"][k]["example"]);
+
+      for(let i=0;i<keyData;i++){
+        Object.keys(data[i]["meanings"]).map((meanings)=>{
+          //console.log(i+"meanings"+meanings);
+          var aux1=0;
+
+          if(aux1==0){
+            if((data[i]["phonetics"][0]["audio"])!=undefined){
+              aux1=1;
+              link="https:"+(data[i]["phonetics"][0]["audio"]);
+              console.log(link);
+
+              document.getElementById("audioLink").setAttribute("src",link);
+              /*
+              var audioEt= document.createElement("audio");
+              document.getElementById("audioApi").appendChild(audioEt);
+
+              //document.getElementById("audioApi").innerHTML="<p>hola</p>"
+              var audioLink= document.createElement("source");
+              audioEt.controls;
+              audioEt.appendChild(audioLink);
+              audioLink.type="audio/mp3"
+              audioLink.src=link
+              */
             }
-        }
-        keyDefinitions=0;
-        break;
-        
-      }
+          }
+          
+          keyMeanings++;
+        })
+        for(let j=0;j<keyMeanings;j++){
+          Object.keys(data[i]["meanings"][j]["definitions"]).map((definitions)=>{
+            if(keyDefinitions==0){
+              var parOfSpeechApi= document.createElement("label");
+              var valueParOfSpeech= document.createElement("output");
+              document.getElementById("textApi").appendChild(parOfSpeechApi);
+              document.getElementById("textApi").appendChild(valueParOfSpeech);
+
+              parOfSpeechApi.innerHTML="<br>"+"Part Of Speech:";
+              valueParOfSpeech.innerHTML=(data[i]["meanings"][j]["partOfSpeech"])+"<br>";
+              console.log("partOfSpeech");
+              console.log(data[i]["meanings"][j]["partOfSpeech"]);
+            }
+            //console.log(i+"definitions"+definitions);
+            keyDefinitions++;
+          })
+          
+          for(let k=0;k<keyDefinitions;k++){
+
+            var definitionsApi= document.createElement("label");
+            var valueDefinitions= document.createElement("output");
+            document.getElementById("textApi").appendChild(definitionsApi);
+            document.getElementById("textApi").appendChild(valueDefinitions);
+    
+            definitionsApi.innerHTML="<br>"+"Definicion:";
+            valueDefinitions.innerHTML=(data[i]["meanings"][j]["definitions"][k]["definition"])+"<br>";
+
+            if(data[i]["meanings"][j]["definitions"][k]["example"]!=undefined){
+              var examplesApi= document.createElement("label");
+              var valueExamples= document.createElement("output");
+              document.getElementById("textApi").appendChild(examplesApi);
+              document.getElementById("textApi").appendChild(valueExamples);
       
+              examplesApi.innerHTML="<br>"+"Example:";
+              valueExamples.innerHTML=(data[i]["meanings"][j]["definitions"][k]["example"])+"<br>";
+            }
 
-    }
+              console.log("Definiciones");
+              console.log(data[i]["meanings"][j]["definitions"][k]["definition"]);
+              if(data[i]["meanings"][j]["definitions"][k]["example"]!=undefined){
+              console.log("Ejemplos");
+              console.log(data[i]["meanings"][j]["definitions"][k]["example"]);
+              }
+          }
+          keyDefinitions=0;
+          break;
+          
+        }
+        
+
+      }
 
 
-  });
-  /*
-  fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word)
-  .then(response =>response.json())
-  .then(data => {
-      console.log(data[0]["meanings"][0]["definitions"][0]["example"]);
-  });
-*/
+    });
+  }else{
+    alert("por favor ingrese un valor primero")
+  }
+    
 }
+
 
 
